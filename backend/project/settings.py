2647246990903c659131f41e36a2e8fc67d4712b
Christian Kedor <christian.kedor@gmail.com>
2022-06-12 22:31:58 -0300

    Backend - Cors Policy

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'rest_framework',
+    'corsheaders',
     'financas'
 ]
 
@@ -89,6 +90,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://127.0.0.1:3000'
 ]
+
 DEFAULT_PERMISSION_CLASSES= ['rest_framework.permissions.IsAuthenticated']
 if DEBUG:
     DEFAULT_PERMISSION_CLASSES = []
