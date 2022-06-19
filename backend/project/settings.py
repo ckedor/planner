3186d38043f6e1e57a4e9cbd0811c1dd4e2d8e14
Christian Kedor <christian.kedor@gmail.com>
2022-06-19 19:14:55 -0300

    Configurations with django-heroku

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -87,6 +87,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = 'static/'
+
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://127.0.0.1:3000'
