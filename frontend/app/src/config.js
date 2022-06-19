efc371e9eb5dfc7197f44b24300f8e56d6cacfe8
Christian Kedor <christian.kedor@gmail.com>
2022-06-19 19:49:19 -0300

    Adicionando URL de prod no front

---
+++ frontend/app/src/config.js
@@ -0,0 +1,10 @@
+const prod = {
+    API_URL: 'https://multi-planner-api.herokuapp.com/'
+}
+
+const dev = {
+    API_URL: 'http://127.0.0.1:8000/'
+}
+
+const config = process.env.NODE_ENV === 'development' ? dev : prod;
+export default config
