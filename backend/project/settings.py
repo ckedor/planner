daccceb3c01beda340545218200fd21e3b3449fe
Christian Kedor <christian.kedor@gmail.com>
2022-08-01 18:27:56 -0300

    backend - ajustes nos settings

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -109,6 +109,7 @@ SIMPLE_JWT = {
 }
     
 REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_FILTER_BACKENDS':['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
